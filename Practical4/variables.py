34243ab3a1600904e15301b545613540b4f6eac0
Lanqi <Lanqi.18@intl.zju.edu.cn>
2019-03-14 09:25:08 +0800

    add variables file

---
+++ Practical4/variables.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:27:27 2019
+
+@author: lanqi
+"""
+#variables
+a = 909
+b = 909909
+print(b%7)
+c = b/7
+d = c/11
+e = d/13
+print(e)
+
+#booleans
+x = True
+y = True
+z = (x and not y)or(y and not x)
+print(z)
+w = x!=y
+print(w)
