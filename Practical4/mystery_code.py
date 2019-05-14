dac78bb9bbf2d8b6bb80ff01be7774db748dd4f6
Lanqi <Lanqi.18@intl.zju.edu.cn>
2019-05-14 16:55:19 +0800

    polish mystery_code

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: to find a random prime numbers from 1 to 100.
+# Answer: to find a prime number from 1 to 100 (and 1 is also included).
 
 # Import libraries
 # randint allows drawing a random number, 
@@ -10,22 +10,27 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
+#this is because if n has a factor bigger than [square root of n], there should be a factor smalle than that.
+#therefore, if a factor cannot be found smaller than [square root of n], n should be a prime number
+
+
 #p is asking whether the number is prime or not
 p=False
-#p is used to control the loop
+#p（Boolean variables) is used to control the loop
 while p==False:
     #p is True than the loop is stopped
     p=True
-    #draw a number between 1 and 100
+    #draw a integer between 1 and 100
     n = randint(1,100)
-    #u is ceiling of root of n to find the factor of n
+    #take square root of n and u is ceiling of the root
+    #it is to find the factor of n
     u = ceil(n**(0.5))
     #to confirm whether n is a prime number
     for i in range(2,u+1):
         if n%i == 0:
             #p is False, then go another loop
             p=False
-
-
+    #if we can find factors of n between 2 and u+1, choose another n
+    #if the n has no factor between 2 and u+1, then n is a prime number
      
 print(n)
