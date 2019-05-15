5be4b67c6353c8a1079b45b11f1cbbd394b2809b
Lanqi <Lanqi.18@intl.zju.edu.cn>
2019-05-15 20:51:56 +0800

    polish Practical8 code

--- Practical8/GO.py
+++ Practical8/GO.py
@@ -14,10 +14,26 @@ DOMTree = xml.dom.minidom.parse("/Users/lanqi/Desktop/IBI1_2018-19/Practical8/go
 #Get the root element of the document
 collection= DOMTree.documentElement
 #a list of elements
-genes=collection.getElementsByTagName("term")
+terms=collection.getElementsByTagName("term")
+
+'''
+Function: CollectChildenGOIDSet (GOID, resultSet)
+    Init: set resultSet to empty before calling the function
+    depends: Global variabe terms - all terms tag of the obo xml
+    
+    for term in terms:
+        get all <is_a> relationship tag, store them in parent_list
+        for parent in parent_list:
+            if parent.id = GOID:
+                (found a child for this GOID)
+                add term in resultSet
+                CollectChildenGOIDSet (term_id, resultSet)
+
+In the main program, count the elements in resultSet
+'''
 #Function to find childNodes
 def CCN(id, resultSet):
-    for gene in genes:
+    for gene in terms:
         parents = gene.getElementsByTagName("is_a")
         gene_id = gene.getElementsByTagName('id')[0].childNodes[0].data
         for parent in parents:
@@ -28,8 +44,9 @@ def CCN(id, resultSet):
 #create a pandas.Dataframe to store the output
 df = pd.DataFrame(columns=['id','name','definition','childnodes'])
 
+
 re_compile= re.compile(r'autophagosome')
-for gene in genes:
+for gene in terms:
     defstr= gene.getElementsByTagName("defstr")[0].childNodes[0].data
     #find terms that contain the word 'autophagosome'
     if re_compile.search(defstr):
@@ -43,6 +60,7 @@ for gene in genes:
         CCN(id, resultSet)
         df = df.append(pd.DataFrame({'id':[id],'name':[name],'definition':[defstr],'childnodes':[len(resultSet)]})) 
         print('childNodes:', len(resultSet))
+
 #save to excel
 res= r'/Users/lanqi/Desktop/IBI1_2018-19/Practical8/autophagosome.xlsx'
 df.to_excel(res,index=False)
